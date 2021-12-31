6a41aaf08741e7a41f10206615707893a8a9589a
gurumojo <theguy@gurumojo.net>
2021-12-31 13:19:10 -0700

    remove engine restrictions

--- spec/test.js
+++ spec/test.js
@@ -1,15 +1,11 @@
 const { ok } = require('assert')
-const { readdir } = require('fs/promises')
+const { readdirSync } = require('fs')
 const { resolve } = require('path')
 
 describe('pipeline', () => {
 
-	it('contains executable feature descriptions', async () => {
-		const path = resolve(__dirname, '../feature')
-		const files = await readdir(path)
-		for (const file of files) {
-			console.log(file)
-		}
+	it('contains executable feature descriptions', () => {
+		const files = readdirSync(resolve(__dirname, '../feature'))
 		ok(!!files.length)
 	})
 })
